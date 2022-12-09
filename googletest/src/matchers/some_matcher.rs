9ed61d045adc40b641593f31ad3e82252dc61b33
Bastien Jacot-Guillarmod <bjacotg@google.com>
2022-12-09 15:24:24 +0000

    Specialize SomeMatcher<_>::explain_match to distinguish between none and some(_) with failing inner matcher.
    Also remove the PhantomData as it is not required anymore.
    
    PiperOrigin-RevId: 494166296

--- googletest/src/matchers/some_matcher.rs
+++ googletest/src/matchers/some_matcher.rs
@@ -14,9 +14,8 @@
 
 #[cfg(not(google3))]
 use crate as googletest;
-use googletest::matcher::{Describe, Matcher, MatcherResult};
+use googletest::matcher::{Describe, MatchExplanation, Matcher, MatcherResult};
 use std::fmt::Debug;
-use std::marker::PhantomData;
 
 /// Matches an `Option` containing a value matched by `inner`.
 ///
@@ -26,23 +25,30 @@ use std::marker::PhantomData;
 /// verify_that!(Some("Some value"), some(eq("Some other value")))?;   // Fails
 /// ```
 pub fn some<T: Debug>(inner: impl Matcher<T>) -> impl Matcher<Option<T>> {
-    SomeMatcher { inner, phantom: Default::default() }
+    SomeMatcher { inner }
 }
 
-struct SomeMatcher<T, InnerMatcherT> {
+struct SomeMatcher<InnerMatcherT> {
     inner: InnerMatcherT,
-    phantom: PhantomData<T>,
 }
 
-impl<T: Debug, InnerMatcherT: Matcher<T>> Matcher<Option<T>> for SomeMatcher<T, InnerMatcherT> {
+impl<T: Debug, InnerMatcherT: Matcher<T>> Matcher<Option<T>> for SomeMatcher<InnerMatcherT> {
     fn matches(&self, actual: &Option<T>) -> MatcherResult {
         actual.as_ref().map(|v| self.inner.matches(v)).unwrap_or(MatcherResult::DoesNotMatch)
     }
-    // TODO(b/261174693) Describe if the value is none or some(x) where x does not
-    // match
+
+    fn explain_match(&self, actual: &Option<T>) -> MatchExplanation {
+        match (self.matches(actual), actual) {
+            (_, Some(t)) => MatchExplanation::create(format!(
+                "which contains {t:?}, {}",
+                self.inner.explain_match(t)
+            )),
+            (_, None) => MatchExplanation::create("which is none".to_string()),
+        }
+    }
 }
 
-impl<T, InnerMatcherT: Describe> Describe for SomeMatcher<T, InnerMatcherT> {
+impl<InnerMatcherT: Describe> Describe for SomeMatcher<InnerMatcherT> {
     fn describe(&self, matcher_result: MatcherResult) -> String {
         match matcher_result {
             MatcherResult::Matches => {
@@ -66,7 +72,7 @@ mod tests {
     #[cfg(not(google3))]
     use googletest::matchers;
     use googletest::{google_test, verify_that, Result};
-    use matchers::eq;
+    use matchers::{contains_substring, displays_as, eq, err};
 
     #[google_test]
     fn some_matches_option_with_value() -> Result<()> {
@@ -93,5 +99,55 @@ mod tests {
         let result = matcher.matches(&None);
 
         verify_that!(result, eq(MatcherResult::DoesNotMatch))
+    }
+
+    #[google_test]
+    fn some_full_error_message() -> Result<()> {
+        let result = verify_that!(Some(2), some(eq(1)));
+        verify_that!(
+            result,
+            err(displays_as(contains_substring(
+                "Value of: Some(2)\n\
+                 Expected: is some(x), where x is equal to 1\n\
+                 Actual: Some(2), which contains 2, which isn't equal to 1"
+            )))
+        )
+    }
+
+    #[google_test]
+    fn some_describe_matches() -> Result<()> {
+        verify_that!(
+            some(eq(1)).describe(MatcherResult::Matches),
+            eq("is some(x), where x is equal to 1")
+        )
+    }
+
+    #[google_test]
+    fn some_describe_does_not_match() -> Result<()> {
+        verify_that!(
+            some(eq(1)).describe(MatcherResult::DoesNotMatch),
+            eq("is none or some(x), where x isn't equal to 1")
+        )
+    }
+
+    #[google_test]
+    fn some_explain_match_with_none() -> Result<()> {
+        verify_that!(some(eq(1)).explain_match(&None), displays_as(eq("which is none")))
+    }
+
+    #[google_test]
+    fn some_explain_match_with_some_success() -> Result<()> {
+        verify_that!(
+            some(eq(1)).explain_match(&Some(1)),
+            displays_as(eq("which contains 1, which is equal to 1"))
+        )
+    }
+
+    #[google_test]
+    fn some_explain_match_with_some_fail() -> Result<()> {
+        verify_that!(
+            some(eq(1)).explain_match(&Some(2)),
+            displays_as(eq("which contains 2, which isn't equal to 1"))
+        )
     }
 }
