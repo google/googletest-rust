88cb3e0009afad0ad90432b18d4c3dd11b204ddb
Bradford Hovinen <hovinen@gmail.com>
2023-05-01 17:06:23 +0200

    Reorganise the docs of the `property!` and `matches_pattern!` macros.
    This change:
     * Moves the admonition about pure functions up to near the top of the documentation, and replaces "Note" with "Important".
     * Adds the same admonition to the documentation of `matches_pattern!`.
     * Adds an example showing that a method returning string slices cannot be used, and removes the blockquote around that part.
     * Clarifies that only string slices are affected by the aforementioned limitation.
     * Adds a test which verifies that array slices are indeed okay.

--- googletest/src/matchers/matches_pattern.rs
+++ googletest/src/matchers/matches_pattern.rs
@@ -44,7 +44,8 @@
 /// #     .unwrap();
 /// ```
 ///
-/// It is not required to include all named fields in the specification:
+/// It is not required to include all named fields in the specification. Omitted
+/// fields have no effect on the output of the matcher.
 ///
 /// ```
 /// # use googletest::{matchers::starts_with, matches_pattern, verify_that};
@@ -136,6 +137,11 @@
 /// #     .unwrap();
 /// ```
 ///
+/// **Important**: The method should be pure function with a deterministic
+/// output and no side effects. In particular, in the event of an assertion
+/// failure, it will be invoked a second time, with the assertion failure output
+/// reflecting the *second* invocation.
+///
 /// These may also include extra parameters you pass in:
 ///
 /// ```
@@ -176,9 +182,6 @@
 /// #    .unwrap();
 /// ```
 ///
-/// > Note: At the moment, this does not work properly with methods returning
-/// > string references or slices.
-///
 /// One can also match tuple structs with up to 10 fields. In this case, all
 /// fields must have matchers:
 ///
@@ -217,14 +220,30 @@
 /// # should_fail().unwrap_err();
 /// ```
 ///
-/// It is perfectly okay to omit fields from a pattern with named fields. The
-/// values of omitted fields then have no effect on the output of the matcher.
-///
 /// This macro does not support plain (non-struct) tuples. Use the macro
 /// [`tuple`] for that purpose.
 ///
 /// Trailing commas are allowed (but not required) in both ordinary and tuple
 /// structs.
+///
+/// Unfortunately, this matcher does *not* work with methods returning string slices:
+///
+/// ```compile_fail
+/// # use googletest::{matchers::eq, property, verify_that};
+/// # #[derive(Debug)]
+/// pub struct MyStruct {
+///     a_string: String,
+/// }
+/// impl MyStruct {
+///     pub fn get_a_string(&self) -> &str { &self.a_string }
+/// }
+///
+/// let value = MyStruct { a_string: "A string".into() };
+/// verify_that!(value, matches_pattern!( MyStruct {
+///     get_a_string(): eq("A string"),   // Does not compile
+/// }))
+/// #    .unwrap();
+/// ```
 #[macro_export]
 macro_rules! matches_pattern {
     ($($t:tt)*) => { $crate::matches_pattern_internal!($($t)*) }
