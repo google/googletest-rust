7b3fa4000761f85de2dc1d9ddee402e48cbc24c2
Bastien Jacot-Guillarmod <bjacotg@google.com>
2023-07-17 01:51:49 -0700

    Switch a `! mr.is_match()` to `mr.is_no_match()`
    PiperOrigin-RevId: 548617918

--- googletest/src/matchers/all_matcher.rs
+++ googletest/src/matchers/all_matcher.rs
@@ -109,7 +109,7 @@ pub mod internal {
                     let failures = self
                         .components
                         .iter()
-                        .filter(|component| !component.matches(actual).is_match())
+                        .filter(|component| component.matches(actual).is_no_match())
                         .map(|component| component.explain_match(actual))
                         .collect::<Description>();
                     if failures.len() == 1 {
